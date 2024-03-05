135020d1d75740a047784ae0e554b5e10c7f7fb0
TechExpand <dailydevo9@gmail.com>
2024-03-05 12:08:04 +0100

    done

--- config/template.ts
+++ config/template.ts
@@ -34,13 +34,7 @@ export const templateEmail = (subject: string, body: string)=>{
                                                 <td style="background-color:#00d2f4;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
                                             </tr>
                                           
-                                            <tr>
-                                                <td style="padding-bottom: 20px;" align="center" valign="top" class="imgHero">
-                                                    <a href="#" style="text-decoration:none" target="_blank">
-                                                        <img alt="" border="0" src="https://foodtruck.express/wp-content/uploads/2023/04/icon.png" style="width:100%;max-width:300px;height:auto;display:block;color: #f9f9f9;" width="300">
-                                                    </a>
-                                                </td>
-                                            </tr>
+                                           
                                             <tr>
                                                 <td style="padding-bottom: 5px; padding-left: 20px; padding-right: 20px;" align="center" valign="top" class="mainTitle">
                                                     <h2 class="text" style="color:#000;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:28px;font-weight:500;font-style:normal;letter-spacing:normal;line-height:36px;text-transform:none;text-align:center;padding:0;margin:0">${subject}</h2>
